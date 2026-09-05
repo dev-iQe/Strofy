c64f61d5c29fc0fcbf67b5baf373f2fa87912130
Personual <bavarite@gmail.com>
2026-09-05 04:43:24 +0300

    Update TMDBService.swift

--- Services/TMDBService.swift
+++ Services/TMDBService.swift
@@ -1,9 +1,7 @@
-import Foundation
-
 class TMDBService: ObservableObject {
     @Published var movies: [Movie] = []
+    @Published var searchResults: [Movie] = []
     
-    // مفتاح API الخاص بك من الصورة
     let apiKey = "12bae60f08973cb30c741d0844769d9d"
     let baseURL = "https://api.themoviedb.org/3"
     
@@ -20,15 +18,24 @@ class TMDBService: ObservableObject {
             }
         }.resume()
     }
-}
-
-// Models/Movie.swift
-struct MovieResponse: Codable { let results: [Movie] }
-struct Movie: Codable, Identifiable {
-    let id: Int
-    let title: String?
-    let name: String? // للمسلسلات
-    let overview: String
-    let poster_path: String?
-    let vote_average: Double
+    
+    func searchMovies(query: String) {
+        guard !query.isEmpty else {
+            DispatchQueue.main.async { self.searchResults = [] }
+            return
+        }
+        
+        let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        guard let url = URL(string: "\(baseURL)/search/multi?api_key=\(apiKey)&language=ar&query=\(encodedQuery)") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(MovieResponse.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.searchResults = decodedResponse.results
+                    }
+                }
+            }
+        }.resume()
+    }
 }
