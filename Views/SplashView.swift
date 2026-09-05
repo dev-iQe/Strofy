7cc409c053fd9aa7a3be5241f16db0dc0455023f
Personual <bavarite@gmail.com>
2026-09-05 04:32:41 +0300

    Update SplashView.swift

--- Views/SplashView.swift
+++ Views/SplashView.swift
@@ -3,17 +3,16 @@ import AVKit
 
 struct SplashView: View {
     @State private var isActive = false
-    // اسم ملف الفيديو الذي سترفعه "video.mov"
     let player = AVPlayer(url: Bundle.main.url(forResource: "video", withExtension: "mov")!)
-    
+
     var body: some View {
         if isActive {
             MainTabView()
         } else {
             ZStack {
-                Color(red: 0.95, green: 0.95, blue: 0.95).edgesIgnoringSafeArea(.all) // أبيض غامق
-                VideoPlayer(player: player)
-                    .disabled(true)
+                Color.black.edgesIgnoringSafeArea(.all)
+                FullScreenVideoPlayer(player: player)
+                    .edgesIgnoringSafeArea(.all)
                     .onAppear {
                         player.play()
                         DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
@@ -22,5 +21,36 @@ struct SplashView: View {
                     }
             }
         }
+    }
+}
+
+// مشغل فيديو يملأ الشاشة بالكامل بدون حواف
+struct FullScreenVideoPlayer: UIViewRepresentable {
+    let player: AVPlayer
+
+    func makeUIView(context: Context) -> PlayerUIView {
+        return PlayerUIView(player: player)
+    }
+
+    func updateUIView(_ uiView: PlayerUIView, context: Context) {}
+}
+
+class PlayerUIView: UIView {
+    private let playerLayer = AVPlayerLayer()
+
+    init(player: AVPlayer) {
+        super.init(frame: .zero)
+        playerLayer.player = player
+        playerLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
     }
 }
