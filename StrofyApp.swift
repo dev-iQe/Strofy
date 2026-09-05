7474c5125a1780a7fc1b979ee7265215d76f3dbf
Personual <bavarite@gmail.com>
2026-09-05 03:50:23 +0300

    Create StrofyApp with SplashView as initial screen
    Initial setup of the StrofyApp with a main entry point and a splash screen.

---
+++ StrofyApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct StrofyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView() // يبدأ بشاشة البداية التي تحتوي على الفيديو
+        }
+    }
+}
